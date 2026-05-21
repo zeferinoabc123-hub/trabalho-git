1f642bc2efa253edae4d927db0bedfb1d629f65c
Daniel S Zeferino <zeferinoabc123@gmail.com>
2026-05-20 21:22:01 -0300

    mudei para .java kk

---
+++ trabalho.java
@@ -0,0 +1,7 @@
+public class trabalho {
+    public static void main(String[] args) {
+        // ESTE PROGRAMA SERVE PARA DAR UM OI;
+
+        System.out.println("eu primeiro (Daniel): Oi");
+    }
+}
