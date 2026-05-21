83a7191e06fba80f3e3fcca98973e73c47be31da
Daniel S Zeferino <zeferinoabc123@gmail.com>
2026-05-20 21:49:54 -0300

    v1

--- trabalho.java
+++ trabalho.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class trabalho {
     public static void main(String[] args) {
-        // ESTE PROGRAMA SERVE PARA DAR UM OI;
+        // CALCULADORA DE MEDIA
+        Scanner entrada = new Scanner(System.in);
+        double media1;
+        System.out.println(""" 
+            ================================
+            * CALCULE SUA NOTA DO SEMESTRE *
+            ================================
 
-        System.out.println("eu primeiro (Daniel): Oi");
+                 """);
+        System.out.print("Digite a nota do 1° bimestre");
+        
     }
 }
