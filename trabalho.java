7a3ce8a2ae38a2d766c589b241472b68cc6eaeaa
Daniel S Zeferino <zeferinoabc123@gmail.com>
2026-05-20 21:55:50 -0300

    v2

--- trabalho.java
+++ trabalho.java
@@ -5,6 +5,7 @@ public class trabalho {
         // CALCULADORA DE MEDIA
         Scanner entrada = new Scanner(System.in);
         double media1;
+        double media2;
         System.out.println(""" 
             ================================
             * CALCULE SUA NOTA DO SEMESTRE *
@@ -12,6 +13,16 @@ public class trabalho {
 
                  """);
         System.out.print("Digite a nota do 1° bimestre");
-        
+            try {
+                media1 = entrada.nextDouble();
+            } catch (Exception e) {
+                media1 = 0.0;
+            }
+        System.out.println("\n Digite a nota do 2° bimestre");
+        try {
+                media2 = entrada.nextDouble();
+            } catch (Exception e) {
+                media2 = 0.0;
+            }
     }
 }
