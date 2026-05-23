00b3115ad5b9a428cf0a9272d631bf310e662e59
Daniel S Zeferino <zeferinoabc123@gmail.com>
2026-05-23 11:21:56 -0300

    v6

---
+++ README.md
@@ -0,0 +1,13 @@
+# Atividade Prática Fundamentos da Computação
+**Código de Validação:** [AUTH-GIT-JAVA-2026]
+
+## Equipe:
+- Daniel dos Santos (R.A.: 12620431)
+- luiz soares (R.A.: 12620394)
+
+## Turma:
+- [x] ADS (Análise e Desenvolvimento de Sistemas)
+- [ ] SI (Sistemas de Informação)
+
+## Descrição:
+Projeto simples de cálculo de média desenvolvido em colaboração via Git para treinar commits e sincronização linear na branch 
--- trabalho.java
+++ trabalho.java
@@ -6,26 +6,33 @@ public class trabalho {
         Scanner entrada = new Scanner(System.in);
         double media1;
         double media2;
+        double media;
         System.out.println(""" 
             ================================
             * CALCULE SUA NOTA DO SEMESTRE *
             ================================
 
                  """);
-        System.out.print("Digite a nota do 1° bimestre\n>>");
+        System.out.print("Digite a nota do 1° bimestre\n>> ");
             try {
-                media1 = entrada.nextDouble();                                        //Dev A
+                media1 = entrada.nextDouble();                                        //Daniel e luiz
             } catch (Exception e) {
                 media1 = 0.0;
             }
-        System.out.println("\nDigite a nota do 2° bimestre\n>>");
+        System.out.print("\nDigite a nota do 2° bimestre\n>> ");
         try {
                 media2 = entrada.nextDouble();
             } catch (Exception e) {
                 media2 = 0.0;
             }
-
-            System.out.println("=== NOTA FINAL ===");
-            System.out.println((media1 + media2) / 2);
+        media = (media1 + media2) / 2;
+        System.out.println("\n=== NOTA FINAL ===");
+        System.out.println(">> " + media);
+        if(media >= 7){
+            System.out.println("Aprovado");
+        }else{
+            System.out.println("Reprovado");
+        }
+        
     }
 }
